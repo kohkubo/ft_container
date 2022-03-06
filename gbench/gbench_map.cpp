ab0822828edffa6b444be08c0860e49cd6b56139
kohkubo <kohkubo@student.42tokyo.jp>
2022-03-07 05:23:00 +0900

    ベンチを追加

--- gbench/gbench_map.cpp
+++ gbench/gbench_map.cpp
@@ -25,3 +25,17 @@ static void BM_MapRemove(benchmark::State& state) {
 }
 BENCHMARK(BM_MapRemove)->Range(1, MAX_RANGE_SIZE);
 
+static void BM_MapOperator(benchmark::State& state) {
+  std::srand(std::time(0));
+  USE_LIB::map<int, int> m;
+  for (int i = 0; i < state.range(0); ++i) {
+    m.insert(USE_LIB::make_pair(std::rand(), std::rand()));
+  }
+  for (auto _ : state) {
+    for (int i = 0; i < state.range(0); ++i) {
+      //benchmark::DoNotOptimize(m[std::rand()]);
+      benchmark::DoNotOptimize(m[std::rand()] = std::rand());
+    }
+  }
+}
+BENCHMARK(BM_MapOperator)->Range(1, MAX_RANGE_SIZE);

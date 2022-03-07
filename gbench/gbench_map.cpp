74dd9793e46e48b590bf55ce2b41002c500ad375
kohkubo <kohkubo@student.42tokyo.jp>
2022-03-07 13:55:56 +0900

    テストケースを改善

--- gbench/gbench_map.cpp
+++ gbench/gbench_map.cpp
@@ -14,12 +14,14 @@ BENCHMARK(BM_MapInsert)->Range(1, MAX_RANGE_SIZE);
 static void BM_MapRemove(benchmark::State& state) {
   std::srand(std::time(0));
   USE_LIB::map<int, int> m;
-  for (int i = 0; i < state.range(0); ++i) {
-    m.insert(USE_LIB::make_pair(std::rand(), std::rand()));
+  for (int i = -state.range(0); i < state.range(0); ++i) {
+    int key = i;
+    m.insert(USE_LIB::make_pair(key, key));
   }
   for (auto _ : state) {
     for (int i = 0; i < state.range(0); ++i) {
-      m.erase(std::rand());
+      int key = std::rand() % state.range(0);
+      benchmark::DoNotOptimize(m.erase(key));
     }
   }
 }

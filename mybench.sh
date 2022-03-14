6d8c898b97e501abac27d44f0c47267d167167de
kohkubo <kohkubo@student.42tokyo.jp>
2022-03-14 22:29:56 +0900

    gawkを使わないように変更

--- mybench.sh
+++ mybench.sh
@@ -3,6 +3,6 @@ set -eu
 rm -rf bench.log bench_std.log
 make mybench > bench.log
 make mybench_std > bench_std.log
-paste bench.log bench_std.log | gawk '{printf "scale=2; %d/%d\n", $5, $2}' | bc &> tmp.log
-paste bench.log tmp.log | gawk '{printf "%-35s %s\n", $1, $4}'
+paste bench.log bench_std.log | awk '{printf "scale=2; %d/%d\n", $5, $2}' | bc &> tmp.log
+paste bench.log tmp.log | awk '{printf "%-35s %s\n", $1, $4}'
 rm -rf tmp.log
